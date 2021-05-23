5f36d201cd66a76462d578722f3d2c55bca4f04a
Biraj Silwal <birajsilwal.ai@gmail.com>
2021-05-22 23:48:40 -0600

    nodemon bodyparser

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+
+const { User } = require('./model/user')
 
 mongoose.connect('mongodb+srv://birajsilwal:Tempmongo123@cluster0.of3id.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
     {useNewUrlParser: true})
     .then(() => console.log('Connected'))
     .catch(err => console.log(err));
 
-app.get('/', function (req, res) {
-  res.send('hello world')
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  res.json("biraj")
 })
+
+app.post('/api/users/register', (req, res) => {
+  const user = new User(req.body)
+  user.save((err, userData) => {
+    if(err) return res.json ({ success:false, err})
+  })
+  return res.status(200).json({
+    success:true
+  })
+})
+
+
 
 app.listen(5000);
