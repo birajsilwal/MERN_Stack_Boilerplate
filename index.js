85ecbd27e6fbbcfddf646ce20652d40eb34f8401
Biraj Silwal <birajsilwal.ai@gmail.com>
2021-05-23 14:05:39 -0600

    environment variables config

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
-
+const config = require('./config/key')
 const { User } = require('./model/user')
 
-mongoose.connect('mongodb+srv://birajsilwal:Tempmongo123@cluster0.of3id.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
+mongoose.connect(config.mongoURI, 
     {useNewUrlParser: true})
-    .then(() => console.log('Connected'))
+    .then(() => console.log('Connected.......'))
     .catch(err => console.log(err));
 
 app.use(bodyParser.urlencoded({extended:true}));
